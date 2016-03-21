3bfbaaf97a02eae70840315441c5088ce495b430
Przemek94 <xxl94@o2.pl>
2016-03-21 12:13:43 +0100

    Rozszerzenie bazy pytań do 10
    Rozszerzenie bazy pytań. Użytkownik po wciśnięciu przycisku „Nowa Gra”
    przechodzi do ekranu Rozpoczynającego grę gdzie po wciśnięciu przycisku
    „Start” uruchamia się pierwsze pytanie. Użytkownik po udzieleniu
    odpowiedzi sam przechodzi do kolejnego pytania za pośrednictwem
    przycisku „Dalej”. Może również nie udzielić żadnej odpowiedzi aby
    przejść do kolejnego pytania. Po udzieleniu jednej odpowiedzi nie ma
    możliwości wybrania innej.

--- app/src/main/java/com/example/ziom/jtr/Start.java
+++ app/src/main/java/com/example/ziom/jtr/Start.java
@@ -4,13 +4,18 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
-public class Menu extends AppCompatActivity {
+public class Start extends AppCompatActivity {
+
+    Button b1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu);
+        setContentView(R.layout.activity_start);
+        b1 = (Button) findViewById(R.id.button4);
+        b1.setBackgroundColor(0xff2BBCEC);
 
         android.support.v7.app.ActionBar bar = getSupportActionBar();
         if (bar != null) {
@@ -18,20 +23,15 @@ public class Menu extends AppCompatActivity {
         }
     }
 
-    public void New_Game(View view) {
-        Intent intent = new Intent(this, Start.class);
+    public void Start(View view) {
+        Intent intent = new Intent(this, Pytanie1.class);
         startActivity(intent);
     }
 
-    public void Help(View view) {
-        Intent intent = new Intent(this, Pomoc.class);
-        startActivity(intent);
+    @Override
+    public void onPause() {
+        super.onPause();
+        finish();
+
     }
-
-    public void Wynik(View view) {
-        Intent intent = new Intent(this, Wynik.class);
-        startActivity(intent);
-    }
-
-
 }
