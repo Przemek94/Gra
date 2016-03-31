627cc6ab7dcd0c49a3be8174b77cff1613dcbcc9
Przemek94 <xxl94@o2.pl>
2016-03-31 17:48:27 +0200

    Rozszerzenie bazy pytań do 100

--- app/src/main/java/com/example/ziom/jtr/Pytanie80.java
+++ app/src/main/java/com/example/ziom/jtr/Pytanie80.java
@@ -110,7 +110,7 @@ public class Pytanie80 extends AppCompatActivity {
     }
 
     public void Dalej(View view) {
-        Intent intent = new Intent(this, Pytanie80.class);
+        Intent intent = new Intent(this, Pytanie81.class);
         startActivity(intent);
     }
 
