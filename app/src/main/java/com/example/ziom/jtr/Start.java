1304dc1fac3925fb090b6b2c6ae54047fdd9af8d
Przemek94 <xxl94@o2.pl>
2016-04-04 08:59:39 +0200

    Dodoanie losowości pierwszego pytania
    Po wciśnięciu "Start" losuje się pierwsze pytanie z puli 20 pytań

--- app/src/main/java/com/example/ziom/jtr/Start.java
+++ app/src/main/java/com/example/ziom/jtr/Start.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.Random;
+
 public class Start extends AppCompatActivity {
 
     Button b1;
@@ -24,9 +26,84 @@ public class Start extends AppCompatActivity {
     }
 
     public void Start(View view) {
-        Intent intent = new Intent(this, Pytanie1.class);
+
+
+        Random generator = new Random();
+        int number = generator.nextInt(20) + 1;
+        //20- liczba losowanych activity
+
+        Class activity = null;
+
+
+        switch(number) {
+            case 1:
+
+                activity = Pytanie1.class;
+                break;
+            case 2:
+                activity = Pytanie2.class;
+                break;
+            case 3:
+                activity = Pytanie3.class;
+                break;
+            case 4:
+                activity = Pytanie4.class;
+                break;
+            case 5:
+                activity = Pytanie5.class;
+                break;
+            case 6:
+                activity = Pytanie6.class;
+                break;
+            case 7:
+                activity = Pytanie7.class;
+                break;
+            case 8:
+                activity = Pytanie8.class;
+                break;
+            case 9:
+                activity = Pytanie9.class;
+                break;
+            case 10:
+                activity = Pytanie10.class;
+                break;
+            case 11:
+                activity = Pytanie11.class;
+                break;
+            case 12:
+                activity = Pytanie12.class;
+                break;
+            case 13:
+                activity = Pytanie13.class;
+                break;
+            case 14:
+                activity = Pytanie14.class;
+                break;
+            case 15:
+                activity = Pytanie15.class;
+                break;
+            case 16:
+                activity = Pytanie16.class;
+                break;
+            case 17:
+                activity = Pytanie17.class;
+                break;
+            case 18:
+                activity = Pytanie18.class;
+                break;
+            case 19:
+                activity = Pytanie19.class;
+                break;
+            default:
+                activity = Pytanie20.class;
+                break;
+        }
+        // We use intents to start activities
+        Intent intent = new Intent(getBaseContext(), activity);
         startActivity(intent);
     }
+
+
 
     @Override
     public void onPause() {
@@ -35,3 +112,4 @@ public class Start extends AppCompatActivity {
 
     }
 }
+
