76c239ba812ac31d2dfea312b92ac7752e34fe3e
Przemek94 <xxl94@o2.pl>
2016-04-14 10:12:10 +0200

    Dodaniu upływu czasu do wszystkich pytań

--- app/src/main/java/com/example/ziom/jtr/Pytanie61.java
+++ app/src/main/java/com/example/ziom/jtr/Pytanie61.java
@@ -1,18 +1,30 @@
 package com.example.ziom.jtr;
 
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
 import android.content.Intent;
 import android.media.MediaPlayer;
+import android.os.Build;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+@TargetApi(Build.VERSION_CODES.GINGERBREAD)
+@SuppressLint("NewApi")
 
 public class Pytanie61 extends AppCompatActivity {
 
     MediaPlayer pytanie;
-    Button b1, b2, b3, b4, b5;
+    Button b1,b2, b3, b4, b5;
+    TextView textViewTime;
+    final CounterClass timer = new CounterClass(15000, 1000);
+
 
 
 
@@ -33,7 +45,7 @@ public class Pytanie61 extends AppCompatActivity {
         b3 = (Button) findViewById(R.id.bttn3);
         b4 = (Button) findViewById(R.id.bttn4);
         b5 = (Button) findViewById(R.id.bttn5);
-
+        textViewTime = (TextView) findViewById(R.id.textViewTime);
         pytanie = MediaPlayer.create(this, R.raw.pytanie61);
         pytanie.start();
         b1.setBackgroundColor(0xff2BBCEC);
@@ -41,6 +53,16 @@ public class Pytanie61 extends AppCompatActivity {
         b3.setBackgroundColor(0xff2BBCEC);
         b4.setBackgroundColor(0xff2BBCEC);
         b5.setBackgroundColor(0xff2BBCEC);
+        b1.setEnabled(false);
+
+        textViewTime.setText("15");
+
+
+
+        timer.start();
+
+
+
 
 
         android.support.v7.app.ActionBar bar = getSupportActionBar();
@@ -57,6 +79,8 @@ public class Pytanie61 extends AppCompatActivity {
                 b2.setEnabled(false);
                 b3.setEnabled(false);
                 b4.setEnabled(false);
+                timer.cancel();
+                b1.setEnabled(true);
             }
 
         });
@@ -71,6 +95,8 @@ public class Pytanie61 extends AppCompatActivity {
                 b5.setEnabled(false);
                 b3.setEnabled(false);
                 b4.setEnabled(false);
+                timer.cancel();
+                b1.setEnabled(true);
             }
 
         });
@@ -85,6 +111,8 @@ public class Pytanie61 extends AppCompatActivity {
                 b5.setEnabled(false);
                 b2.setEnabled(false);
                 b4.setEnabled(false);
+                timer.cancel();
+                b1.setEnabled(true);
             }
 
         });
@@ -97,10 +125,52 @@ public class Pytanie61 extends AppCompatActivity {
                 b2.setEnabled(false);
                 b5.setEnabled(false);
                 b3.setEnabled(false);
+                timer.cancel();
+                b1.setEnabled(true);
 
             }
 
         });
+
+
+    }
+
+    @TargetApi(Build.VERSION_CODES.GINGERBREAD)
+    @SuppressLint("NewApi")
+    public class CounterClass extends CountDownTimer {
+
+        public CounterClass(long millisInFuture, long countDownInterval) {
+            super(millisInFuture, countDownInterval);
+            // TODO Auto-generated constructor stub
+        }
+
+        @SuppressLint("NewApi")
+        @TargetApi(Build.VERSION_CODES.GINGERBREAD)
+        @Override
+        public void onTick(long millisUntilFinished) {
+            // TODO Auto-generated method stub
+
+            long millis = millisUntilFinished;
+            String hms = String.format("%02d", TimeUnit.MILLISECONDS.toSeconds(millis));
+            System.out.println(hms);
+            textViewTime.setText(hms);
+        }
+
+        @Override
+        public void onFinish() {
+            // TODO Auto-generated method stub
+            pytanie.stop();
+            b4.setBackgroundColor(0xFF00FF00);
+            textViewTime.setText("Koniec Czasu");
+            b1.setEnabled(true);
+            b2.setEnabled(false);
+            b3.setEnabled(false);
+            b4.setEnabled(false);
+            b5.setEnabled(false);
+        }
+
+
+
     }
     public void Dalej(View view) {
         Random generator = new Random();
@@ -183,6 +253,8 @@ public class Pytanie61 extends AppCompatActivity {
     public void onPause() {
         super.onPause();
         finish();
+        timer.cancel();
+
 
     }
 
