ab169f3aadb2293326151cdaf4dd39cc1ac21478
Przemek94 <xxl94@o2.pl>
2016-05-05 21:03:00 +0200

    Revert "Najlepszy wynik - wersja poprawiona"
    This reverts commit d0476052f0910146a8ac465ab723fd751caf1e33.

--- app/src/main/java/com/example/ziom/jtr/Koniec.java
+++ app/src/main/java/com/example/ziom/jtr/Koniec.java
@@ -1,21 +1,16 @@
 package com.example.ziom.jtr;
 
 import android.content.Intent;
-import android.content.IntentSender;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.TextView;
-import android.content.SharedPreferences;
 
 import java.util.concurrent.TimeUnit;
 
 public class Koniec extends AppCompatActivity {
 
-    TextView Wynik, NajleszyWynik;
-    long punkty, punkty1, ppunkty;
-    public static final String SAVE = "Najlepszy Wynik";
-    Intent intent;
+    TextView Wynik;
+    long punkty;
 
 
     @Override
@@ -23,34 +18,12 @@ public class Koniec extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_koniec);
         Wynik = (TextView) findViewById(R.id.Wynik);
-        NajleszyWynik = (TextView) findViewById(R.id.NajlepszyWynik);
 
         Wynik.setText("punkty");
 
         Intent getpytanie;
         getpytanie = getIntent();
         punkty = getpytanie.getLongExtra("Punkty", 0);
-
-        ppunkty = punkty;
-
-
-
-        SharedPreferences LNW = getSharedPreferences(SAVE, 0);
-        punkty1 = LNW.getLong("Punkty", 0);
-        if(ppunkty>punkty1){
-            String wynik =  String.format("%02d", ppunkty);
-            System.out.println(wynik);
-            NajleszyWynik.setText(wynik);
-            punkty1 = ppunkty;
-            SharedPreferences SNW = getSharedPreferences(SAVE, MODE_PRIVATE);
-            SharedPreferences.Editor editor = SNW.edit();
-            editor.putLong("Punkty", punkty1);
-            editor.apply();
-        }
-        else
-            NajleszyWynik.setText(String.valueOf(punkty1));
-
-
 
 
         String wynik =  String.format("%02d", punkty);
@@ -59,18 +32,10 @@ public class Koniec extends AppCompatActivity {
 
 
 
+
         android.support.v7.app.ActionBar bar = getSupportActionBar();
         if (bar != null) {
             bar.hide();
         }
-    }
-
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        finish();
-
-
     }
 }
--- app/src/main/java/com/example/ziom/jtr/MainActivity.java
+++ app/src/main/java/com/example/ziom/jtr/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         Thread watek = new Thread(){
             public void run(){
                 try {
-                    sleep(3000);
+                    sleep(5000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
